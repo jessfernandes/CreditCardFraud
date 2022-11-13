4df20caa6c0e76d54b1851e33407ec77ebdbde41
senai <jessica.lopes@sistemafiep.org.br>
2022-11-13 00:51:11 -0300

    Update: superuser

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,7 +7,7 @@ echo "-----> Migrating"
 python manage.py migrate
 
 echo "-----> Creating super user"
-python manage.py shell < create_users.py    
+python manage.py shell < ./scripts/create_users.py    
 
 echo "-----> Starting server"
 python manage.py runserver 0.0.0.0:$APPLICATION_PORT
